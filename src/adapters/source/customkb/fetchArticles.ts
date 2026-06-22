4aaffc55275bf4f946fc5609da54d67d1440ef83
chiragshrivastava-ui <chirag.shrivastava@davies-group.com>
2026-06-22 12:11:43 +0530

    Create fetchArticles.ts

---
+++ src/adapters/source/customkb/fetchArticles.ts
@@ -0,0 +1,13 @@
+import fs from "fs";
+import path from "path";
+
+export async function fetchArticles() {
+  const dir = path.join(process.cwd(), "articles");
+
+  const files = fs.readdirSync(dir);
+
+  return files.map(file => {
+    const content = fs.readFileSync(path.join(dir, file), "utf-8");
+    return JSON.parse(content);
+  });
+}
