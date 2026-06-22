8d09a5ae2d129144e9358df3f04bbe9c36e5453f
chiragshrivastava-ui <chirag.shrivastava@davies-group.com>
2026-06-22 12:20:35 +0530

    Update customkb.ts

--- src/configurers/customkb.ts
+++ src/configurers/customkb.ts
@@ -1,18 +1,19 @@
 import { pipe } from "../pipe.js";
 import { fetchArticles } from "../adapters/source/customkb/fetchArticles.js";
-import { transformToGenesysFormat } from "../processors/transformToGenesysFormat.js";
 
 export default function customkbConfigurer() {
   pipe.addStep(async (context) => {
-    // STEP 1: Fetch articles from Git repo
+
     const articles = await fetchArticles();
 
-    // STEP 2: Transform them to Genesys format
-    const transformed = articles.map(article =>
-      transformToGenesysFormat(article)
-    );
+    const transformed = articles.map(article => ({
+      externalId: article.id,
+      title: article.title,
+      content: {
+        body: article.content
+      }
+    }));
 
-    // Attach to pipeline context
     context.documents = transformed;
 
     return context;
