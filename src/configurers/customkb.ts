0c259e80c9dad02db0ef371cf50c6610bbf5cf83
chiragshrivastava-ui <chirag.shrivastava@davies-group.com>
2026-06-24 12:41:06 +0530

    Update customkb.ts

--- src/configurers/customkb.ts
+++ src/configurers/customkb.ts
@@ -1,14 +1,13 @@
 import { fetchArticles } from "../adapters/source/customkb/fetchArticles";
 
 export default async function customkbConfigurer() {
-
   const articles = await fetchArticles();
 
-  return articles.map(article => ({
-    externalId: article.id,
+  return articles.map((article) => ({
+    externalId: article.externalId,   // ✅ FIXED
     title: article.title,
     content: {
-      body: article.content
+      body: article.content.body
     }
   }));
 }
